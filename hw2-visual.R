e89bd355594ae863fa9bc55e58a59d571bc967af
Yiyi-Dai <159573586+Yiyi-Dai@users.noreply.github.com>
2024-02-19 18:41:19 -0500

    Update hw2-visual.R
    update the dataset

--- hw2-visual.R
+++ hw2-visual.R
@@ -5,6 +5,19 @@ data<-data.frame(hw1_data)
 library(ggplot2)
 library(GGally)
 
+#or
+library(data.table)
+data <- data.frame(
+  GPA = c(3.75,4.0,3.5,3.75,3.6,3.2,2.8,3.75,4.0,3.9,3.4,3.0,3.0,3.9,3.8,3.3,3.4,3.75,3.9,3.5,3.75,3.9,4.0,3.2,3.75,3.8,3.6,3.1,3.3,2.9),
+  English_Level = c(92, 100, 89, 98, 89, 94, 84, 102, 110, 105, 93, 90, 85, 99, 83, 90, 91, 100, 106, 95, 98, 94, 105, 85, 99, 99, 90, 92, 93, 86),
+  Social_Integration = c(5, 2, 1, 3, 2, 3, 10, 7, 5, 4, 4, 9, 1, 1, 3, 2, 2, 5, 4, 3, 4, 3, 3, 9, 3, 4, 4, 1, 1, 1),
+  Study_Hours_Per_Week = c(15, 20, 1, 12, 6, 6, 5, 18, 12, 14, 10, 8, 4, 19, 10, 5, 6, 10, 15, 13, 14, 16, 18, 6, 10, 12, 16, 4, 3, 2),
+  Mental_Health = c(4, 3, 3, 4, 2, 3, 2, 4, 4, 5, 3, 4, 1, 2, 2, 4, 3, 3, 4, 2, 5, 4, 5, 1, 3, 4, 2, 1, 2, 1),
+  Social_Media_Hours_Per_Week = c(20, 10, 18, 16, 13, 20, 15, 16, 12, 22, 15, 24, 5, 4, 15, 25, 23, 15, 18, 15, 13, 16, 12, 24, 18, 12, 14, 28, 13, 22),
+  Gaming_Hours_Per_Week = c(10, 6, 5, 10, 8, 3, 15, 8, 2, 5, 14, 10, 12, 7, 5, 16, 18, 8, 5, 2, 13, 5, 4, 12, 6, 8, 12, 14, 15, 20),
+  Sport_Hours_Per_Week = c(2, 10, 40, 14, 7, 7, 14, 5, 3, 12, 10, 0, 8, 0, 2, 8, 10, 9, 8, 0, 2, 3, 5, 0, 5, 7, 7, 1, 2, 0)
+)
+
 #Univariate visualization
 #How is the GPA distributed in all the samples?
 ggplot(data, aes(x=GPA)) + geom_bar(fill="steelblue")
@@ -45,6 +58,8 @@ scatterplotMatrix(~English_Level+Social_Integration+Study_Hours_Per_Week+Mental_
 GPA, data=data, var.labels=labs.data1,cex.labels=0.7, data1="boxplot",smooth=FALSE,reg.line=FALSE,
 pch=c(1,16),col=rep("steelblue",2), legend.plot=FALSE)
 
-#What is the relationship between GPA and English Level, study hours and social integration?
+#What is the relationship between GPA and those independent variables?
 model <- lm(GPA ~ English_Level + Social_Integration + Study_Hours_Per_Week + Mental_Health + Social_Media_Hours_Per_Week + Gaming_Hours_Per_Week + Sport_Hours_Per_Week, data = data)
 summary(model)
+
+
